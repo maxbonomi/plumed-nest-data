a0972c7c2dd547feda89e1985c721256139b5d74
Massimiliano Bonomi <massimiliano.bonomi@gmail.com>
2020-05-09 15:37:03 +0200

    changed names - removed useless stuff

--- projects-2020/EMMI-ASCT2/3-ANALYSIS/3.1-ASSEMBLE-XTC/get_global_xtc.sh
+++ projects-2020/EMMI-ASCT2/3-ANALYSIS/3.1-ASSEMBLE-XTC/get_global_xtc.sh
@@ -1,5 +1,5 @@
-# stride for writing xtc file [ps]
-step=5
+# gromacs stride for writing xtc file [ps]
+nstx=5
 
 # 1) discard initial 20% of trajectory of each replica
 # cycle on all directories
@@ -12,23 +12,21 @@ do
  # count number of frames
  nframes=`gmx check -f tmp.xtc |& grep Step | awk '{print $2}'`
  # set begin
- b=`echo ${nframes} ${step} | awk '{printf "%d\n",$1*$2*0.2}'`
+ b=`echo ${nframes} ${nstx} | awk '{printf "%d\n",$1*$2*0.2}'`
  # discard 20% and (optionally) skip frames with the option -skip 
- gmx trjconv -f tmp.xtc -b $b -o traj_all.xtc 
+ gmx trjconv -f tmp.xtc -b $b -o traj_rep.xtc 
  # clean
  rm tmp.xtc
 done
 
 # 2) cat together the trajectories of all replicas 
-gmx trjcat -f rep-*/traj_all.xtc -cat -o tmp.xtc
-# change timestep and starting time
-gmx trjconv -f tmp.xtc -timestep 1 -t0 0 -o traj_all.xtc
+gmx trjcat -f rep-*/traj_rep.xtc -cat -o traj_all.xtc
 
 # 3) run driver on global trajectory to fix PBCs
 plumed driver --plumed plumed_driver.dat --mf_xtc traj_all.xtc
 
-# 4) convert test.gro to xtc file
-echo 0 | gmx trjconv -f test.gro -o traj_all_PBC.xtc -s ../../0-TOPO/step5_charmm2gmx.pdb -n ../../0-TOPO/index.ndx 
+# 4) convert traj.gro to xtc file
+echo 0 | gmx trjconv -f traj.gro -o traj_all_PBC.xtc -s ../../0-TOPO/step5_charmm2gmx.pdb -n ../../0-TOPO/index.ndx 
 
 # 5) final clean
-rm tmp.xtc traj_all.xtc test.gro rep-*/traj_all.xtc
+rm traj_all.xtc traj.gro rep-*/traj_rep.xtc
